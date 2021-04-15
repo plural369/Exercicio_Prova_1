ca0a2b0077f6947c7861c523f025fba7a356b90e
plural369 <57465395+plural369@users.noreply.github.com>
2021-04-15 13:10:58 -0300

    Completo

---
+++ Exercicio_Prova_1/Exercicio_Prova_1/Controllers/BibliotecaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exercicio_Prova_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercicio_Prova_1.Controllers
+{
+    public class BibliotecaController : Controller
+    {
+
+        private static IList<BibliotecaModel> bibliotecas = new List<BibliotecaModel>()
+        {
+             new BibliotecaModel()
+            {
+                Id = 1,
+                Nome = "Audi a3",
+                Autor = "Azul",
+                Editora = "Stadia",
+                Ano_de_Lançamento = 1000,
+                Genero = "Ação",
+                Idioma = "Português",
+                Introducao = "Era uma vez....."
+
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(bibliotecas);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BibliotecaModel biblioteca)
+        {
+            if (ModelState.IsValid)
+            {
+                bibliotecas.Add(biblioteca);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(biblioteca);
+        }
+    }
+}
---
+++ Exercicio_Prova_1/Exercicio_Prova_1/Models/BibliotecaModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_Prova_1.Models
+{
+    public class BibliotecaModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Editora { get; set; }
+
+        public int Ano_de_Lançamento { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Idioma { get; set; }
+
+        public string Introducao { get; set; }
+
+    }
+}
